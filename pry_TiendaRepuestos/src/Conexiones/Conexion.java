9e4904ec0020de469bb85ef464f516b0c784d567
kref2400 <kevinespinal0024@gmail.com>
2021-10-28 14:20:38 -0600

    conexiones y avance modulo ventas

--- pry_TiendaRepuestos/src/Conexiones/Conexion.java
+++ pry_TiendaRepuestos/src/Conexiones/Conexion.java
@@ -1,14 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+
 package Conexiones;
 
-/**
- *
- * @author fgodo
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
 public class Conexion {
+    private static final String driver = "com.mysql.jdbc.Driver";
+    private static final String user = "root";
+    private static final String pass = "olimpia10";
+    private static final String url = "jdbc:mysql://localhost:3306/bdrepuestos?characterEncoding=utf8";
+    private Connection con = null;
     
+    public static Connection getConexion(Connection con)
+    {
+        if (con == null)
+        {
+            try 
+            {
+                //con = DriverManager.getConnection("jdbc:mysql://192.168.0.120/inventariohospital","root","daniel100"  );
+		con = DriverManager.getConnection(url, user, pass);
+                if(con!=null){
+                    System.out.println("exito");
+                }
+            }
+            catch (SQLException ex) 
+            {
+               
+            }
+        }
+        return con;
+    }
 }
---
+++ pry_TiendaRepuestos/src/Controladores/VentaControlador.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controladores;
+
+public class VentaControlador {
+    private int Tippacodigo;
+    private String TippsDesc;
+
+    public int getTippacodigo() {
+        return Tippacodigo;
+    }
+
+    public void setTippacodigo(int Tippacodigo) {
+        this.Tippacodigo = Tippacodigo;
+    }
+
+    public String getTippsDesc() {
+        return TippsDesc;
+    }
+
+    public void setTippsDesc(String TippsDesc) {
+        this.TippsDesc = TippsDesc;
+    }
+    
+    
+}
